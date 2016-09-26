8dbfb3d08ef155792919ffb66c1da80a1d4cec20
Dustin Younse <milsyobtaf@gmail.com>
2016-09-26 15:17:25 -0400

    terminus 0.13.2

--- Formula/terminus.rb
+++ Formula/terminus.rb
@@ -6,8 +6,8 @@ class Terminus < Formula
 
   desc "Command-line interface for the Pantheon Platform"
   homepage "https://github.com/pantheon-systems/terminus"
-  url "https://github.com/pantheon-systems/terminus/archive/0.13.0.tar.gz"
-  sha256 "edf2312694389b35e39427ea77790623c4bc3a13d57d4d38ac001438786f64dc"
+  url "https://github.com/pantheon-systems/terminus/archive/0.13.2.tar.gz"
+  sha256 "409af570fb955cae113fcc1cabab4b2c9e56ead107bf6e870aedbca0a443cbc4"
   head "https://github.com/pantheon-systems/terminus.git"
 
   bottle do
